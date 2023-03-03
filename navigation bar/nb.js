afc4eb9060e70f68d8ae38ea89f0779b8e1ee0e0
samraTahir <107885308+samraTahir@users.noreply.github.com>
2023-03-03 23:46:50 +0500

    Add files via upload

---
+++ navigation bar/nb.js
@@ -0,0 +1,7 @@
+let bar = document.querySelector(".fa-bars");
+let menu = document.querySelector(".menu");
+let e = function(c) {
+    menu.classList.toggle("show-menu")
+}
+bar.addEventListener('Click' , e );
+
